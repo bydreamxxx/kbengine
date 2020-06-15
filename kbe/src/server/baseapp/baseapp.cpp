181b8ffce33f87fa0e71964f7d9506c229265944
chenweilan <973244297@qq.com>
2020-06-15 17:17:29 +0800

    KBEN-218:程序BUG -- 机器人进程报 “Entity::onUpdatePropertys: not found 143”. base只缓存跳转后的cell的消息，跳转前cell的消息直接发送。

--- kbe/src/server/baseapp/baseapp.cpp
+++ kbe/src/server/baseapp/baseapp.cpp
@@ -4228,6 +4228,14 @@ void Baseapp::forwardMessageToClientFromCellapp(Network::Channel* pChannel,
 
 	BaseMessagesForwardClientHandler* pBufferedSendToClientMessages = pEntity->pBufferedSendToClientMessages();
 
+	 //需要判断来源是否符合
+	if (pBufferedSendToClientMessages)
+	{
+		Components::ComponentInfos* cinfos = Components::getSingleton().findComponent(pChannel);
+		if (cinfos->cid != pBufferedSendToClientMessages->cellappID())
+			pBufferedSendToClientMessages = NULL;
+	}
+
 	Network::Channel* pClientChannel = entitycall->getChannel();
 	Network::Bundle* pSendBundle = NULL;
 	
